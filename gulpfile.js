1aaac803f50d4b9f6e72d29bb6818b9447a33ad5
Lucas Bickel <lucas.bickel@swisscom.com>
2015-08-19 10:22:11 +0200

    Build and deploy stuff

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var gulp = require('gulp');
+
+var $ = require('gulp-load-plugins')({
+    pattern: ['gulp-*', 'del']
+});
+
+gulp.paths = {
+    "dist": "dist",
+    "sources": [
+        "index.html",
+        "bower_components/angular-swagger-ui/dist/css/swagger-ui.min.css",
+        "bower_components/airbrake-js-client/dist/client.js",
+        "bower_components/jquery/dist/jquery.min.js",
+        "bower_components/angular/angular.min.js",
+        "bower_components/angular-swagger-ui/dist/scripts/swagger-ui.min.js",
+        "bower_components/angular-resource/angular-resource.min.js",
+        "bower_components/angular-cookies/angular-cookies.min.js",
+        "bower_components/angular-sanitize/angular-sanitize.min.js",
+    ]
+};
+
+gulp.task('clean', function(done) {
+    $.del([gulp.paths.dist + '/'], done);
+});
+
+gulp.task('build', function(done) {
+    gulp.src(gulp.paths.sources)
+        .pipe($.copy(gulp.paths.dist));
+});
+
+gulp.task('default', ['clean'], function() {
+    gulp.start('build');
+});
